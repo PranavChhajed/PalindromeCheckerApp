f08596c200083d95ab776fe1d0e769f123611b66
Pranav <pranavchhajed842@gmail.com>
2026-02-23 11:15:55 +0530

    UC5_StackBasedPalindromeChecker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,29 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String sr = "Racecar";
         String srlow = sr.toLowerCase();
         char[] chars = srlow.toCharArray();
 
-        if (isPalindrome(chars)) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : chars) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+        for (char c : chars) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println("\"" + sr + "\" is a palindrome");
         } else {
             System.out.println("\"" + sr + "\" is NOT a palindrome");
         }
-    }
-
-    public static boolean isPalindrome(char[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-            if (arr[left] != arr[right]) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
     }
 }
