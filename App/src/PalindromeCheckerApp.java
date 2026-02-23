a810b5d631848f2dc6f77c00f47c3dc46eb29086
Pranav <pranavchhajed842@gmail.com>
2026-02-23 11:07:35 +0530

    UC4_CharacterArrayBasedPalindromeCheck

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,38 +1,28 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        // Hardcoded string
         String sr = "Racecar";
-
-        // Convert to lowercase for proper comparison
         String srlow = sr.toLowerCase();
+        char[] chars = srlow.toCharArray();
 
-        // Reverse the string
-        String reversed = reverseString(srlow);
-
-        // Check if palindrome
-        if (srlow.equals(reversed)) {
+        if (isPalindrome(chars)) {
             System.out.println("\"" + sr + "\" is a palindrome");
         } else {
             System.out.println("\"" + sr + "\" is NOT a palindrome");
         }
     }
 
-    // Function to reverse a string manually
-    public static String reverseString(String str) {
-        char[] chars = str.toCharArray();
+    public static boolean isPalindrome(char[] arr) {
         int left = 0;
-        int right = chars.length - 1;
+        int right = arr.length - 1;
 
         while (left < right) {
-            // Swap characters
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-
+            if (arr[left] != arr[right]) {
+                return false;
+            }
             left++;
             right--;
         }
 
-        return new String(chars);
+        return true;
     }
 }
